73537e36f95b04e6b7736c51d0ff6b9e6e8de17c
ugurozturk <mail@uozturk.com>
2018-10-10 23:40:40 +0300

    Ufak değişiklikler yapıldı

--- wallpaperDownloader/Program.cs
+++ wallpaperDownloader/Program.cs
@@ -2,6 +2,8 @@
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.IO;
 using System.Threading;
@@ -10,14 +12,17 @@ namespace wallpaperDownloader
 {
     internal class Program
     {
+        static string dbName = "..\\..\\myDB.sqlite";
+        //155
         private static void Main(string[] args)
         {
+            dbOlustur();
             ChromeOptions options = new ChromeOptions();
             options.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
             options.AddArguments("headless");
             IWebDriver driver = new ChromeDriver(options);
 
-            int pageNumber = 1;
+            int pageNumber = sonGirilenSayfa();
             while (true)
             {
                 Console.WriteLine("Getting in : https://alpha.wallhaven.cc/search?q=&categories=111&purity=100&atleast=1920x1080&ratios=16x9&sorting=date_added&order=asc&colors=999999&page=" + pageNumber);
@@ -28,6 +33,7 @@ namespace wallpaperDownloader
                 //    Thread.Sleep(10000);
                 //}
                 pageNumber++;
+                sqlYaz("PageNumber", pageNumber.ToString());
 
                 Console.WriteLine("Getting Urls");
                 IList<IWebElement> all = driver.FindElements(By.ClassName("preview"));
@@ -52,6 +58,7 @@ namespace wallpaperDownloader
                             string fileName = System.IO.Path.Combine(rootPath, name + ".png");
                             image.Save(fileName);
                             Console.WriteLine("Saved : " + rootPath + "\\" + name + ".png");
+                            sqlYaz("SavedImage", name);
                         }
                         else
                         {
@@ -90,6 +97,62 @@ namespace wallpaperDownloader
             }
 
             return image;
+        }
+
+        static bool dbOlustur()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + dbName + ";Version=3;"))
+            {
+                conn.Open();
+                string sql = "CREATE TABLE IF NOT EXISTS degerler (key_name varchar(20), key_value TEXT)";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            return true;
+        }
+
+        static bool sqlYaz(string keyname, string keyvalue)
+        {
+
+            //Yoksa oluşturacaktır.
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + dbName + ";Version=3;"))
+            {
+                conn.Open();
+                string sql = $"INSERT INTO degerler(key_name,key_value) VALUES ('{keyname}','{keyvalue}')";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            return true;
+
+        }
+
+        static int sonGirilenSayfa()
+        {
+            int LastPage = 1;
+
+
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + dbName + ";Version=3;"))
+            {
+                conn.Open();
+                string sql = $"select MAX(CAST(key_value AS INT)) as pagenumber from degerler group by key_name having key_name like 'PageNumber'";
+                SQLiteCommand command = conn.CreateCommand();
+
+                command.CommandText = sql;
+                SQLiteDataReader r = command.ExecuteReader();
+                if (r.HasRows)
+                {
+                    while (r.Read())
+                    {
+                        LastPage = Convert.ToInt32(r["pagenumber"]);
+                    }
+                }
+                conn.Close();
+            }
+            return LastPage;
         }
     }
 }
